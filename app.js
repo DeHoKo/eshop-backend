311328ccc563111417b3b733e00c3164ec9c0e93
DeHoKo <zenkovsk@gmail.com>
2020-09-11 02:29:14 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -79,6 +79,15 @@ const goods = [
   },
 ];
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/products", (req, res) => {
   res.json(goods);
 });
